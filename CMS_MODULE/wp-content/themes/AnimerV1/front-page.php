b3e4b2b9f4f2fce4ea3dc9e759aab6c0aa8dd472
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-10-26 14:03:51 +0800

    CMS: commit 2

--- CMS_MODULE/wp-content/themes/AnimerV1/front-page.php
+++ CMS_MODULE/wp-content/themes/AnimerV1/front-page.php
@@ -14,4 +14,29 @@
 <!--            </header>-->
         <?php endwhile; endif; ?>
     </main>
+
+    <?php
+        $query = new WP_Query([
+           'order' => 'DESC',
+            'orderby' => 'date',
+            'post_type' => 'post',
+            'posts_per_page' => '4'
+        ]);
+    ?>
+
+    <div class="recommended">
+        <h2>Recommended</h2>
+        <div id="recommended-list">
+		    <?php while($query->have_posts()): $query->the_post(); ?>
+                <div class="recommended__item">
+                    <h3><?php the_title(); ?></h3>
+                    <a href="">
+                        <img src="<?php the_post_thumbnail_url('thumbnail'); ?>"
+                             alt="<?php the_title(); ?>"
+                        >
+                    </a>
+                </div>
+		    <?php endwhile; ?>
+        </div>
+    </div>
 <?php get_footer(); ?>
