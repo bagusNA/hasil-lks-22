5c24acdc076528befad6c9bb34e915c49b6ef3d8
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-10-26 15:20:28 +0800

    CMS: commit 7

--- CMS_MODULE/wp-content/themes/AnimerV1/functions.php
+++ CMS_MODULE/wp-content/themes/AnimerV1/functions.php
@@ -34,3 +34,25 @@ add_action( 'init', function () {
 		'top'
 	);
 } );
+
+function my_custom_login_logo() {
+	$homeId = get_option('page_on_front');
+	$thumbnailId = get_post_thumbnail_id($homeId);
+	$thumbnailUrl = wp_get_attachment_image_url($thumbnailId, 'large');
+
+	echo '
+	<style>
+         h1 a { display: none !important;
+         height: 120px !important; width: 410px !important; margin-left: -40px;}
+         body {
+         	background-image: url('. $thumbnailUrl .') !important;
+         	background-size: cover;
+         	background-position: center;
+         	backdrop-filter: brightness(.25);
+         }
+         a {
+         color: white !important;
+         }
+     </style>';
+}
+add_action('login_head', 'my_custom_login_logo');
