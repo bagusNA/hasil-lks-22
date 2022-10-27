a60e94e3481a7497fc02e50c0cb40ca9d9085001
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2022-10-27 10:43:21 +0800

    Client: commit 2

--- CLIENT_MODULE/assets/js/Hexaria.js
+++ CLIENT_MODULE/assets/js/Hexaria.js
@@ -27,16 +27,32 @@ export class Hexaria {
 
     init() {
         this.drawGrid();
+        // this.testDrawHexa(10, 20);
+        // this.testDrawGrid(
+        // this.testDrawGrid(this.grid.radius * 16 , 500)
+        //     this.grid.width * this.grid.radius * 2,
+        //     this.grid.width * this.grid.radius * 2
+        // );
+
+        console.log(this.grid.width * this.grid.radius * 2,
+            this.grid.width * this.grid.radius * 2
+        )
     }
 
-    drawHexagon(x, y) {
+    drawHexagon(xGrid, yGrid) {
+        const isEverySecondRow = yGrid % 2 ? true : false;
+        const offsetX = isEverySecondRow ? this.grid.radius / 10 - 2 : 0;
+
+        const isEverySecondCol = xGrid % 2 ? true : false;
+        const offsetY = isEverySecondCol ? this.grid.radius : 0;
+
         this.ctx.strokeStyle = this.color.border;
         this.ctx.beginPath();
 
         for (let i = 0; i < 6; i++) {
             this.ctx.lineTo(
-                (x * this.grid.radius * 2) + this.grid.radius * Math.cos(this.angle * i),
-                (y * this.grid.radius * 2) + this.grid.radius * Math.sin(this.angle * i)
+                (xGrid * this.grid.radius * 2 + offsetX) + this.grid.radius * Math.cos(this.angle * i),
+                (yGrid * this.grid.radius * 2 + offsetY) + this.grid.radius * Math.sin(this.angle * i)
             )
         }
 
@@ -56,6 +72,26 @@ export class Hexaria {
                 this.drawHexagon(rowIndex + 1, colIndex + 1);
             });
         });
+    }
 
+    testDrawHexa(x, y) {
+        this.ctx.strokeStyle = this.color.border;
+        this.ctx.beginPath();
+        for (let i = 0; i < 6; i++) {
+            this.ctx.lineTo(
+                x + this.grid.radius * Math.cos(this.angle * i),
+                y + this.grid.radius * Math.sin(this.angle * i)
+            );
+        }
+        this.ctx.closePath();
+        this.ctx.stroke();
+    }
+
+    testDrawGrid(width, height) {
+        for (let y = this.grid.radius; y + this.grid.radius * Math.sin(this.angle) < height; y += this.grid.radius * Math.sin(this.angle)) {
+            for (let x = this.grid.radius, j = 0; x + this.grid.radius * (1 + Math.cos(this.angle)) < width; x += this.grid.radius * (1 + Math.cos(this.angle)), y += (-1) ** j++ * this.grid.radius * Math.sin(this.angle)) {
+                this.testDrawHexa(x, y);
+            }
+        }
     }
 }
